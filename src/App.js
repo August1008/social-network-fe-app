25598f90638aee9dfd5a9780c65953f384db8b48
August1008 <ngbinhan1512@gmail.com>
2023-05-10 20:26:49 +0700

    Update to add login page.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Registration from './Components/Registration';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Components/Login';
 function App() {
   return (
     <div className="App">
-      <Registration/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Registration/>}></Route>
+          <Route path='/login' element={<Login/>}></Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
